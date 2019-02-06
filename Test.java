58f43cad5f7afc7816c1d613dfb16a3a1932e0c3
sreevatsavbv <36199936+sreevatsavbv@users.noreply.github.com>
2019-02-06 21:49:47 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,12 @@
+package com.realdetect.webcam;
+
+import java.io.IOException;
+
+public class Test {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		  new FaceDetector().detectFace();
+	}
+
+}
